aedaf0312d77717218374196c71b988f42bdd8f9
josephsap <joesap1@gmail.com>
2016-09-15 20:41:09 -0600

    second commit

--- js/scripts.js
+++ js/scripts.js
@@ -15,50 +15,5 @@ $(document).ready(function(){
     $("body").removeClass("noscroll");
   });
 
-  // bike hover squares
-  $('.left img').hover(function() {
-    var data = $(this).data('closeup');
-    $('#rt-big').attr('src', data);
-    $('#caption').html($(this).data('caption'));
-   });
-
-//   function checkWidth() {
-//     var $winSz = $(window).width();
-
-//     var $main = $('.main');
-//     var $intro = $('.intro');
-//     var $occ = $('.occupation');
-//     var $name = $('.name')
-
-//     if($winSz >=961) {
-//       $main.removeClass('center-responsive');
-//       $intro.removeClass('intro-mobile');
-//       $occ.removeClass('occupation-mobile');
-//       $name.removeClass('name-mobile');
-//     }
-
-//     if(($winSz >=961) && ($winSz <= 641)) {
-//       $main.removeClass('center-responsive');
-//       $intro.removeClass('intro-mobile');
-//       $occ.removeClass('occupation-mobile');
-//       $name.removeClass('name-mobile');
-//     }
-
-//     if($winSz <= 960) {
-//       $main.addClass('center-responsive');
-//       $intro.addClass('intro-mobile');
-//       $occ.addClass('occupation-mobile');
-//       $name.addClass('name-mobile');
-//     }
-
-//     if($winSz <=640) {
-//       $main.removeClass('center-responsive');
-
-//     }
-// }
-
-// checkWidth();
-// $(window).resize(checkWidth);
-
 
 }); // end doc ready
